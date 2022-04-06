2cb11911a6f6ae5abd727516870c8713117893d2
PaulModaley <pmodaley@gmail.com>
2022-04-06 14:45:55 +0000

    cart app and template working

--- music_store/urls.py
+++ music_store/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/profile/', views.profile),
     path('products/', include('products.urls')),
     path('', include('home.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
