90c734c458cb2801cb4ee056b2b8a313c97ae864
PaulModaley <pmodaley@gmail.com>
2022-04-11 17:19:09 +0000

    created model for user wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+# model for wishlist
+class UserWishlist(models.Model):
+    wishlist_name = models.CharField(max_length=30,null=True,blank=True)
+    wishlist_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class WishlistProduct(models.Model):
+    wishlist = models.ForeignKey(UserWishlist, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
