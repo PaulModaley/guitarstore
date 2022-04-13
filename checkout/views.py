c9d0d68fa075652925c4305e106877dda62106bd
PaulModaley <pmodaley@gmail.com>
2022-04-13 15:03:23 +0000

    began adding stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ko7AHHeYwWa7liY3QKjZ319f0mIyhVamdn2oDvTqFidVEgFN14cnV36mwVVtzt7698DlhIdv4KJ5sdUNnaqInvf00gsdUpY4R',
+        'client_secret': 'sk_test_51Ko7AHHeYwWa7liYtIpy2e81Pp9759wlyfwEP5m0IUhTX8uw5ELkvXtBbj0PAgk7vle8scoC69gjareAA3vIqGWg00ocSR7R5N',
     }
 
     return render(request, template, context)
