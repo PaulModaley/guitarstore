837db775967579d0fbf4bc3cb4d101a9dbe662eb
PaulModaley <pmodaley@gmail.com>
2022-04-21 19:26:56 +0000

    add toasts including css, html, js etc

--- cart/views.py
+++ cart/views.py
@@ -14,6 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
