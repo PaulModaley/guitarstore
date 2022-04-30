0eedc249010d1cc5e626de18660c8adb1963a8b7
PaulModaley <pmodaley@gmail.com>
2022-04-30 19:10:39 +0000

    add urls, view, forms and template for contact but not working at present - template doesn't exist

--- music_store/urls.py
+++ music_store/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('checkout/', include('checkout.urls')),
     path('accounts/profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
