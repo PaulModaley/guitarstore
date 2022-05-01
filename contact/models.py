141560071541882c98648cb0bc008a3fe56cd173
PaulModaley <pmodaley@gmail.com>
2022-05-01 16:10:23 +0000

    refine contact form

--- contact/models.py
+++ contact/models.py
@@ -4,5 +4,5 @@ from django.forms import ModelForm
 class Contact(models.Model):
     name = models.CharField(max_length= 100)
     email = models.EmailField(max_length= 250)
-    subject = models.CharField(max_length=40, default=True)
+    subject = models.CharField(max_length=40)
     message = models.CharField(max_length=500)
