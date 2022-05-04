053cc67035f848f4c2b5fd68d7f5c8e2e8a0a72d
PaulModaley <pmodaley@gmail.com>
2022-05-04 12:16:32 +0000

    add code to forms.py file

---
+++ newslettter/forms.py
@@ -0,0 +1,10 @@
+# import form class from django
+from django import forms
+from .models import Contact
+
+# create a ModelForm
+class Add_Subscriber(forms.ModelForm):
+	# specify the name of model to use
+	class Meta:
+		model = Newsletter
+		fields = "__all__"
