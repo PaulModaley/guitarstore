4e62ac0dbf28713e28e5d3e1ab9606420b2f6ed8
PaulModaley <pmodaley@gmail.com>
2022-05-05 12:09:01 +0000

    start adding views and urls and html form for newsletter

--- newslettter/views.py
+++ newslettter/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import Add_Subscriber
+from .models import Newsletter
 
 # Create your views here.
+def add_subscriber(request):
+    form = Add_Subscriber
+    return render(request, 'newslettter/add_subscriber')
