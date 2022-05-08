e899c183a72e47ec68a8c336c03c78b478d19178
PaulModaley <pmodaley@gmail.com>
2022-05-08 12:43:19 +0000

    add style newsletter form on home page

--- newsletter/views.py
+++ newsletter/views.py
@@ -12,26 +12,9 @@ def newsletter(request):
     form = Subscriber(request.POST or None)
     if form.is_valid():
         form.save()
+        messages.success(request, 'Thank you for subscribing')
 
     context = {
         'form' : form
     }
-    return render(request, "newsletter/newsletter.html", context)
-
-
-# def newsletter(request):
-
-# 	context ={}
-
-# 	# create object of form
-# 	form = Subscribe(request.POST or None, request.FILES or None)
-	
-# 	# check if form data is valid
-# 	if form.is_valid():
-# 		# save the form data to model
-# 		form.save()
-# 		messages.success(request, 'Your message has been sent!')
-#             # redirect to contact page
-        
-
-# 	template = 'newsletter/newsletter.html'
+    return render(request, "home/index.html", context)
