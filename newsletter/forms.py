914aa38e23b611039943028a728a1246d8221076
PaulModaley <pmodaley@gmail.com>
2022-05-08 12:18:03 +0000

    finally, newsletter subscribes is working!

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Subscribe
 
 # create a ModelForm
-class Subscribe(forms.ModelForm):
+class Subscriber(forms.ModelForm):
 	# specify the name of model to use
 	class Meta:
 		model = Subscribe
--- newsletter/views.py
+++ newsletter/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
-from .forms import Subscribe
+from .forms import Subscriber
 from .models import Subscribe
 
 from django.shortcuts import render
 # Create your views here.
-def newsletter(request):
-    """ A view to return the index page """
 
-    return render(request, 'newsletter/newsletter.html')
+def newsletter(request):
+    form = Subscriber(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form' : form
+    }
+    return render(request, "newsletter/newsletter.html", context)
+
+
+# def newsletter(request):
+
+# 	context ={}
+
+# 	# create object of form
+# 	form = Subscribe(request.POST or None, request.FILES or None)
+	
+# 	# check if form data is valid
+# 	if form.is_valid():
+# 		# save the form data to model
+# 		form.save()
+# 		messages.success(request, 'Your message has been sent!')
+#             # redirect to contact page
+        
+
+# 	template = 'newsletter/newsletter.html'
