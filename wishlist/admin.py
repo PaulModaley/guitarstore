da98f9e9540826881fb1a4d999d2f29347465539
PaulModaley <pmodaley@gmail.com>
2022-06-15 19:37:31 +0000

    tidy up code and remove commented out code from all files

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,9 +1,3 @@
-# from django.contrib import admin
-# from .models import UserWishlist, WishlistProduct
-
-# # Register your models here.
-# admin.site.register(UserWishlist)
-# admin.site.register(WishlistProduct)
 from django.contrib import admin
 from wishlist.models import WishList
 
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,18 +1,3 @@
-# from django.db import models
-# from django.conf import settings
-# from products.models import Product
-# from django.contrib.auth.models import User
-
-# # model for wishlist
-# class UserWishlist(models.Model):
-#     wishlist_name = models.CharField(max_length=30,null=True,blank=True)
-#     wishlist_owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-# class WishlistProduct(models.Model):
-#     wishlist = models.ForeignKey(UserWishlist, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.IntegerField(default=0)
-
 from django.db import models
 
 from profiles.models import UserProfile
